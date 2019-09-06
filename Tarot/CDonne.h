296a0164af484ac936bc131b4bf648888ba5ea14
Léo Noraz <leo.noraz@free.fr>
2019-09-06 14:11:05 +0200

    Beta 1

--- Tarot/CDonne.h
+++ Tarot/CDonne.h
@@ -1,1 +1,21 @@
 #pragma once
+enum contrat { passe, prise, garde, garde_sans, garde_contre };
+enum camp { preneur, personne, defenseur };
+enum poignee { sans, simple, doublee, triplee, autre };
+enum chelem { sans, reussi, perdu, sans_annonce };
+
+class CDonne
+{
+private:
+	contrat type_contrat;
+	int nb_points;
+	int nb_bouts;
+	int points;
+	camp camp_poignee;
+	poignee type_poignee;
+	camp camp_petit_au_bout;
+	chelem type_chelem;
+public:
+	CDonne(int nbpts, int bts, int pts, camp cmp_pgn, camp petit, contrat type, chelem typ_chlm, poignee typ_pgn);
+	~CDonne();
+};
