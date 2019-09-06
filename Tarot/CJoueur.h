31ed6cd5f3c5103f7126201da6f9380b99ed118e
wullepit <wullepit@A6>
2019-09-06 13:58:47 +0200

    0.2

--- Tarot/CJoueur.h
+++ Tarot/CJoueur.h
@@ -1,1 +1,32 @@
 #pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+class CJoueur
+{
+private:
+	string nom;
+	int score;
+public:
+	string lireNom();
+	int lireScore();
+	int majScore();
+};
+
+string CJoueur::lireNom()
+{
+	//A coder
+}
+
+int CJoueur::lireScore()
+{
+	//A coder
+}
+
+int CJoueur::majScore()
+{
+	//A coder
+}
